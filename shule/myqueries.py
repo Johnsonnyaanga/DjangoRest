9bdba3ad4a69d16948921c4b7ee3e027aa5ccf65
unknown <johnsonyaanga@gmail.com>
2022-01-23 16:12:18 +0300

    queries

--- shule/myqueries.py
+++ shule/myqueries.py
@@ -10,12 +10,20 @@ teachers = Teacher.objects.all()
 parents = Parent.objects.all()
 
 #filter students by parent id
+#get all students of parent 1
+students = Parent.objects.get(id=1).student_set.all()
 students = Student.objects.all().filter(id=1)
+
+#get parent by student
+stud1 = Student.objects.first()
+parent_name = stud1.parent.parent_name
 
 #filter students by class id
 students = Student.objects.all().filter(class_level=1)
 
 #filter assignments by class id
 assignments =Assignment.objects.all().filter(assignment_class=1)
+
+
 
 
